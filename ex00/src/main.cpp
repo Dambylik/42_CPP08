269580f17124bdeb08ec3de1c7fb9f4cbe108457
okapshai <okapshai@student.42.fr>
2025-03-24 12:15:10 +0100

    deque added

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: okapshai <okapshai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 14:42:08 by okapshai          #+#    #+#             */
-/*   Updated: 2025/03/24 12:03:10 by okapshai         ###   ########.fr       */
+/*   Updated: 2025/03/24 12:14:51 by okapshai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,34 @@ int main() {
     try {
         std::list<char>::iterator itchar = easyfind(letters, 'e');
         std::cout << FGRN("Element found in container: ") << *itchar << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << FRED(e.what()) << std::endl;
+    }
+
+    std::cout << std::endl << "--------------DEQUE--------------" << std::endl;
+    std::deque<int> fnumbers;
+    fnumbers.push_back(10);
+    fnumbers.push_back(20);
+    fnumbers.push_back(30);
+    fnumbers.push_back(40);
+    fnumbers.push_back(50);
+
+    std::cout << FYEL("Deque INT created: ");
+    for (std::deque<int>::iterator itf = fnumbers.begin(); itf != fnumbers.end(); ++itf) {
+        std::cout << *itf << " ";
+    }
+    std::cout << std::endl;
+    
+    try {
+        std::deque<int>::iterator itint = easyfind(fnumbers, 30);
+        std::cout << FGRN("Element found in container: ") << *itint << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << FRED(e.what()) << std::endl;
+    }
+
+    try {
+        std::deque<int>::iterator itint = easyfind(fnumbers, 100);
+        std::cout << FGRN("Element found in container: ") << *itint << std::endl;
     } catch (const std::exception& e) {
         std::cout << FRED(e.what()) << std::endl;
     }
